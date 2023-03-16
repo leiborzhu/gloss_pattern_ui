12c9c8758eb5edb1a73a943df19ab4c29495344b
leiborzhu <84162319+leiborzhu@users.noreply.github.com>
2023-03-16 17:42:03 +0800

    Update Alise.py

--- Alise.py
+++ Alise.py
@@ -48,3 +48,14 @@ fadeInAlise_R_4 = 'R_fade_in_F_4'
 startAlise_R_4 = 'R_start_F_4'
 endAlise_R_4 = 'R_end_F_4'
 fadeOutAlise_R_4 = 'R_fade_out_4'
+max_frameAlise = 'max_frame'
+optional_label = [patterAlise_L_1, fadeInAlise_L_1, startAlise_L_1, endAlise_L_1, fadeOutAlise_L_1,
+                  patterAlise_L_2, fadeInAlise_L_2, startAlise_L_2, endAlise_L_2, fadeOutAlise_L_2,
+                  patterAlise_L_3, fadeInAlise_L_3, startAlise_L_3, endAlise_L_3, fadeOutAlise_L_3,
+                  patterAlise_L_4, fadeInAlise_L_4, startAlise_L_4, endAlise_L_4, fadeOutAlise_L_4,
+                  patterAlise_R_1, fadeInAlise_R_1, startAlise_R_1, endAlise_R_1, fadeOutAlise_R_1,
+                  patterAlise_R_2, fadeInAlise_R_2, startAlise_R_2, endAlise_R_2, fadeOutAlise_R_2,
+                  patterAlise_R_3, fadeInAlise_R_3, startAlise_R_3, endAlise_R_3, fadeOutAlise_R_3,
+                  patterAlise_R_4, fadeInAlise_R_4, startAlise_R_4, endAlise_R_4, fadeOutAlise_R_4,
+
+                  ]
